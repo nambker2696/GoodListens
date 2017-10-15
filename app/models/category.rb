a179beab8532fcc034e5967a0f5c1e55c4abd09d
nhatan172 <nhatan172@gmail.com>
2017-10-16 00:47:51 +0700

    16/10

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
 	has_many :rel_songs, class_name: "RelSongCategory",
-		foreign_key: "id"
+		foreign_key: "category_id", primary_key: "id"
 	has_many :songs, through: :rel_songs, source: :songs
 
 	def self.get_popular_categories
