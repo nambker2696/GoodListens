7374e6944ce7fc1a891a15313ff9e0eca0c35021
nambker2696 <nguyenvannam20132696@gmail.com>
2017-11-03 16:04:42 +0700

    Edit man hinh

--- db/migrate/20170930082349_create_songs.rb
+++ db/migrate/20170930082349_create_songs.rb
@@ -6,6 +6,7 @@ class CreateSongs < ActiveRecord::Migration[5.1]
       t.integer :author_id
       t.integer :sum_rate
       t.float :rate_avg
+      t.string :slug
       t.string :cover, default: "/assets/images/fallback/default_song.jpg"
       t.string :song_url, default: ""
       t.integer :album_id
