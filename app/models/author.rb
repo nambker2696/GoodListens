6a8a6aa00c7b3ee4827e536aa4a87b87b3d58833
nambker2696 <nguyenvannam20132696@gmail.com>
2017-12-04 21:11:38 +0700

    Phien ban cuoi cung

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
 	has_many :songs, dependent: :destroy
-	# mount_uploader :avatar, AvatarUploader
+	mount_uploader :avatar, AvatarUploader
 	enum gender: %i(Female Male).freeze
 end
